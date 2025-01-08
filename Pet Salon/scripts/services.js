8f660e87f065a53f5e575ec7440e63c84a8b0c1c
Brandon Merle <brandon.merle@yahoo.com>
2025-01-07 19:59:11 -0500

    Add files via upload

--- Pet Salon/scripts/services.js
+++ Pet Salon/scripts/services.js
@@ -1,16 +1,31 @@
 //using jQuery
-//constuctor
-function Service(description, price){
 
+document.getElementById("results");
+$("#results");
+
+let servicesForm = $("#servicesForm").css("border", "1px solid black",);
+
+//constuctor
+class Service {
+    constructor(description, price) {
+        this.description = description;
+        this.price = price;
+    }
 }
 //register
 function register(){
+    let description = $("#descriptionInput").val();
+    $("#results").append(`<li>${description}</li`);
+    let price = $("#priceInput").val();
+    $("#results").append(`<li>${price}</li`);
     console.log("A new service was added");
+
 }
 
 function init(){
     $("#registerBtn").on('click', register);
-
+    
 }
+
 
 window.onload=init;
